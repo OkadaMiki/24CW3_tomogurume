7732c1672e3c30b84af56f0192f735045966a4e2
OkadaMiki <okadamiki17@gmail.com>
2024-08-01 14:17:12 +0900

    mapのapiにつないだだけ

---
+++ js/map.js
@@ -0,0 +1,14 @@
+function initMap() {
+    // 地図の中心を設定
+    const location = { lat: 34.707019092306, lng: 135.50543410838 }; // 東京の緯度と経度
+    // 地図を表示
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: location,
+    });
+    // マーカーを表示
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+}
